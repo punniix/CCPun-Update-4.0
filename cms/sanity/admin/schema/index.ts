83129fb1b5c2edc398cc03e518031c8f0cf6051f
punniix <chanatip.chid@gmail.com>
2026-09-11 20:26:14 +0700

    refactor(scheduling): remove unused Sanity operational schedule schema registration

--- cms/sanity/admin/schema/index.ts
+++ cms/sanity/admin/schema/index.ts
@@ -1,9 +1,7 @@
 import { adminSchemaTypes } from "./admin-types";
-import { publishSchedule } from "./publish-schedule";
 import { ubersuggestSchemaTypes } from "./ubersuggest-types";
 
 export const adminIntelligenceSchemaTypes = [
   ...adminSchemaTypes,
   ...ubersuggestSchemaTypes,
-  publishSchedule,
 ];
