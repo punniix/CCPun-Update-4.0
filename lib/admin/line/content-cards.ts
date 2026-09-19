aa42317e2c9067ebbf67c8b43e6dd958a852090b
punniix <chanatip.chid@gmail.com>
2026-09-19 20:28:20 +0700

    feat(line): use Sanity curation for live article cards

--- lib/admin/line/content-cards.ts
+++ lib/admin/line/content-cards.ts
@@ -6,13 +6,20 @@ import {
   selectLineDiscoveryArticles,
 } from "../../line/content-cards";
 import type { LineJourneyId } from "../../line/ecosystem";
+import { readLineDiscoveryCuration } from "./discovery-config";
 
 export async function readPublishedLineDiscoveryArticles(journey: LineJourneyId) {
-  const articles = await sanityContentProvider.listArticles({ includeDrafts: false });
-  return selectLineDiscoveryArticles(journey, articles);
+  const [articles, curation] = await Promise.all([
+    sanityContentProvider.listArticles({ includeDrafts: false }),
+    readLineDiscoveryCuration(),
+  ]);
+  return selectLineDiscoveryArticles(journey, articles, curation.journeys[journey]);
 }
 
 export async function buildPublishedLineArticleFlexMessage(journey: LineJourneyId) {
-  const articles = await sanityContentProvider.listArticles({ includeDrafts: false });
-  return buildLineArticleFlexMessage(journey, articles);
+  const [articles, curation] = await Promise.all([
+    sanityContentProvider.listArticles({ includeDrafts: false }),
+    readLineDiscoveryCuration(),
+  ]);
+  return buildLineArticleFlexMessage(journey, articles, curation.journeys[journey]);
 }
